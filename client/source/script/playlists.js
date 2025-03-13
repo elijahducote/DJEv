f1038a085a20dc9f21c8607ee670b516956b60eb
Elijah Ducote <validpupflower@gmail.com>
2025-03-13 11:46:53 -0500

    Update

--- client/source/script/playlists.js
+++ client/source/script/playlists.js
@@ -45,7 +45,7 @@ export function Playlists() {
       "br"
     ),
     
-    htm(htm(undefined,"iframe",{src: "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F1749603915&show_artwork=true&show_comments=false",style: "width: 175%; height: 100%; position: absolute; border: 0px;",allowfullscreen:"",scrolling: "no"}),
+    htm(htm(undefined,"iframe",{src: "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F1504987279&show_artwork=true&secret_token=s-cLlbzYSiZH8",style: "width: 175%; height: 100%; position: absolute; border: 0px;",allowfullscreen:"",scrolling: "no"}),
     
     "div",
       {
